2db5e98094b8d6585a2b71296634579101e48f92
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-07-14 19:57:52 +0200

    feat: create show and destroy endpoints

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,6 +12,18 @@ module Api
         )
         render json: { user: user }, status: :ok
       end
+
+      def show
+        user = User.find(params[:id])
+
+        render json: { user: user }, status: :ok
+      end
+
+      def destroy
+        user = User.destroy(params[:id])
+
+        render json: { message: 'User has been deleted!' }, status: :ok
+      end
     end
   end
 end
