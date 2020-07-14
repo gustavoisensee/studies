afe8c2d1ef827eb807f99fb1dbc35a28e6fead35
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-07-14 21:37:57 +0200

    chore: cleaning

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class UsersController < ApplicationController
       def index
-        render json: { users: User.all }, status: :ok
+        users = User.all;
+        render json: users, status: :ok
       end
 
       def create
@@ -10,18 +11,16 @@ module Api
           first_name: params[:first_name],
           last_name: params[:last_name]
         )
-        render json: { user: user }, status: :ok
+        render json: user, status: :ok
       end
 
       def show
         user = User.find(params[:id])
-
-        render json: { user: user }, status: :ok
+        render json: user, status: :ok
       end
 
       def destroy
-        user = User.destroy(params[:id])
-
+        User.destroy(params[:id])
         render json: { message: 'User has been deleted!' }, status: :ok
       end
     end
