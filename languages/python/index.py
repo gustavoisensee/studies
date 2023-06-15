9922a7ab2a1b609df076e39bcc6881939ea25e9c
Gustavo Isensee <gustavo.isensee@gmail.com>
2023-06-15 08:51:24 +0200

    Create ruby and python folder with index file to start my studies on that

---
+++ languages/python/index.py
@@ -0,0 +1,6 @@
+# Print a simple message
+print("Hello, World!")
+
+# Perform a simple calculation
+result = 10 + 5
+print("The result is:", result)
---
+++ languages/ruby/index.rb
@@ -0,0 +1,6 @@
+# Print a simple message
+puts "Hello, World!"
+
+# Perform a simple calculation
+result = 10 + 5
+puts "The result is: #{result}"
