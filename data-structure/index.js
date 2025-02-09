c6c39befbf86e3f1250d896ddd66585d4dff005c
Gustavo Isensee <gustavo.isensee@gmail.com>
2025-02-09 18:09:29 +0100

    feat: add flatten object function

--- data-structure/index.js
+++ data-structure/index.js
@@ -1,7 +1,7 @@
 const graph = require('./src/graph');
 const { dfs } = require('./src/dfs');
 const { bfsShortestPath, minStops } = require('./src/bfs');
-const { factorial, findLastChild } = require('./src/recursion');
+const { factorial, findLastChild, flattenObject } = require('./src/recursion');
 
 // #### BFS - Breath first search #################################
 console.log('shortest path: ', bfsShortestPath('A', 'F', graph));
@@ -17,3 +17,26 @@ console.log('Factorial of 5 is ', factorial(5)); // Output: 120
 // ### Recursion - findLastChild ##################################
 const tree = { name: 'root', children: [{ name: 'child 1', children: [{ name: 'child 2', children: [] }] }] }
 console.log('Last child is ', findLastChild(tree)); // Output: child 2
+
+// ### Flatten Object #############################################
+const nestedObject = {
+  name: "Gus",
+  age: 36,
+  address: {
+    city: "Utrecht",
+    zip: { code: 3533, extension: 'CH' },
+  },
+  hobbies: ["reading", { type: "game", name: "FFXIV" }],
+};
+console.log('Flatten Object', flattenObject(nestedObject));
+// Output:
+// {
+//   name: 'Gus',
+//   age: 36,
+//   'address.city': 'Utrecht',
+//   'address.zip.code': 3533,
+//   'address.zip.extension': 'CH',
+//   'hobbies[0]': 'reading',
+//   'hobbies[1].type': 'game',
+//   'hobbies[1].name': 'FFXIV'
+// }
--- data-structure/src/recursion.js
+++ data-structure/src/recursion.js
@@ -12,7 +12,34 @@ function findLastChild(node) {
   return findLastChild(node.children[node.children.length - 1]);
 }
 
+function flattenObject(obj, prefix = '') {
+  let result = {};
+
+  for (let key in obj) {
+    const newKey = prefix ? `${prefix}.${key}` : key;
+
+    if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      result = { ...result, ...flattenObject(obj[key], newKey) };
+
+    } else if (Array.isArray(obj[key])) {
+      obj[key].forEach((item, index) => {
+        if (typeof item === 'object' && item !== null) {
+          result = {...result, ...flattenObject(item, `${newKey}[${index}]`)};
+        } else {
+          result[`${newKey}[${index}]`] = item;
+        }
+      });
+
+    } else {
+      result[newKey] = obj[key];
+    }
+  }
+
+  return result;
+}
+
 module.exports = {
   factorial,
-  findLastChild
+  findLastChild,
+  flattenObject
 }
